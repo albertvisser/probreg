4b5073ab158b0005f056cd6d529150c8ca3d5188
albert <albert@lemoncurry>
2011-09-18 14:20:42 +0200

    docstring toegevoegd; data in dictioanries op juiste formaat

--- probreg/config.py
+++ probreg/config.py
@@ -1,30 +1,41 @@
+"""config.py - configuratie voor probreg sql versie
+
+definieert een aantal constanten en de default waarden voor de settings dictionaries:
+DBLOC - waar de database staat
+USER - default userid om in te vullen waar nodig
+APPS - waar de lijst met apps staat
+kopdict - dictionary van tabs in de vorm                 volgorde: (tab titel,  link naam)
+statdict: dictionary van mogelijke statussen in de vorm  volgorde: (omschrijving, code, id in tabel)
+catdict: dictionary van mogelijke soorten in de vorm     volgorde: (omschrijving, code, id in tabel)
+"""
 DBLOC = "/home/albert/www/actiereg/actiereg.db"
 USER = 2
 APPS = "/home/albert/www/actiereg/apps.dat"
-# default settings
+
 kopdict = {
-    "0": ("Lijst",'index'),
-    "1": ("Titel/Status",'detail'),
-    "2": ("Probleem/Wens",'meld'),
-    "3": ("Oorzaak/Analyse",'oorz'),
-    "4": ("Oplossing/SvZ",'opl'),
-    "5": ("Vervolgactie",'verv'),
-    "6": ("Voortgang",'voortg')
-}
-statdict = {
-    "0": ("gemeld", 0),
-    "1": ("in behandeling", 1),
-    "2": ("oplossing controleren",2),
-    "3": ("nog niet opgelost",3),
-    "4": ("afgehandeld", 4),
-    "5": ("afgehandeld - vervolg",5)
-}
-catdict = {
-    "P": ("probleem",1),
-    "W": ("wens",2),
-    " ": ("onbekend",0),
-    "V": ("vraag",3),
-    "I": ("idee",4),
-    "F": ("div. informatie",5)
+    "0": ("Lijst", 'index'),
+    "1": ("Titel/Status", 'detail'),
+    "2": ("Probleem/Wens", 'meld'),
+    "3": ("Oorzaak/Analyse", 'oorz'),
+    "4": ("Oplossing/SvZ", 'opl'),
+    "5": ("Vervolgactie", 'verv'),
+    "6": ("Voortgang", 'voortg')
 }
 
+statdict = {
+    "0": ("gemeld", 0, -1),
+    "1": ("in behandeling", 1, -1),
+    "2": ("oplossing controleren", 2, -1),
+    "3": ("nog niet opgelost",3, -1),
+    "4": ("afgehandeld", 4, -1),
+    "5": ("afgehandeld - vervolg", 5, -1)
+}
+
+catdict = {
+    "P": ("probleem", 1, -1),
+    "W": ("wens", 2, -1),
+    " ": ("onbekend", 0, -1),
+    "V": ("vraag", 3, -1),
+    "I": ("idee", 4, -1),
+    "F": ("div. informatie", 5, -1)
+}
