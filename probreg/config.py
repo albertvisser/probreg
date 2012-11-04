a9eee40f3d600156ecd5ce9f9369f499a8954662
Albert Visser <albert@lemoncurry>
2012-11-04 22:41:53 +0100

    config.py voor xml versie

---
+++ probreg/config.py
@@ -0,0 +1,25 @@
+kopdict = {
+    "0": "Lijst",
+    "1": "Titel/Status",
+    "2": "Probleem/Wens",
+    "3": "Oorzaak/Analyse",
+    "4": "Oplossing",
+    "5": "Vervolgactie",
+    "6": "Voortgang"
+}
+statdict = {
+    "0": ("gemeld", 0),
+    "1": ("in behandeling", 1),
+    "2": ("oplossing controleren",2),
+    "3": ("nog niet opgelost",3),
+    "4": ("afgehandeld", 4),
+    "5": ("afgehandeld - vervolg",5)
+}
+catdict = {
+    "P": ("probleem",1),
+    "W": ("wens",2),
+    "": ("onbekend",0),
+    "V": ("vraag",3),
+    "I": ("idee",4),
+    "F": ("div. informatie",5)
+}
