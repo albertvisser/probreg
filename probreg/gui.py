78a55265017b17b446a7f91810c1f270837c38b1
Albert Visser <albert@lemoncurry>
2019-09-20 23:14:18 +0200

    #768 added more classes to import

--- probreg/gui.py
+++ probreg/gui.py
@@ -4,8 +4,10 @@ redirect gui-insensitive import to gui-sensitive module
 """
 from .toolkit import toolkit
 if toolkit == 'qt':
-    from .gui_qt import MainGui
+    from .gui_qt import MainGui, PageGui, Page0Gui, \
+        show_message, get_open_filename, get_save_filename, get_choice_item
 elif toolkit == 'wx':
-    from .gui_wx import MainGui
+    from .gui_wx import MainGui, PageGui, Page0Gui, \
+        show_message, get_open_filename, get_save_filename, get_choice_item
 else:
     raise ValueError('Unknown GUI-toolkit specified')
