8967edc2b1139740d6e69ae218f5f8c85d9f9cee
Albert Visser <albert@lemoncurry>
2019-10-05 08:47:40 +0200

    #768 import toegevoegd

--- probreg/gui.py
+++ probreg/gui.py
@@ -4,13 +4,13 @@ redirect gui-insensitive import to gui-sensitive module
 """
 from .toolkit import toolkit
 if toolkit == 'qt':
-    from .gui_qt import MainGui, PageGui, Page0Gui, Page1Gui, \
-        SortOptionsDialog, SelectOptionsDialog, LoginBox, \
+    from .gui_qt import MainGui, PageGui, Page0Gui, Page1Gui, Page6Gui, \
+        SortOptionsDialog, SelectOptionsDialog, SettOptionsDialog, LoginBox, \
         show_message, ask_cancel_question, get_open_filename, get_save_filename, \
         get_choice_item, show_dialog
 elif toolkit == 'wx':
-    from .gui_wx import MainGui, PageGui, Page0Gui, Page1Gui, \
-        SortOptionsDialog, SelectOptionsDialog, LoginBox, \
+    from .gui_wx import MainGui, PageGui, Page0Gui, Page1Gui, Page6Gui, \
+        SortOptionsDialog, SelectOptionsDialog, SettOptionsDialog, LoginBox, \
         show_message, ask_cancel_question, get_open_filename, get_save_filename, \
         get_choice_item, show_dialog
 else:
